a66877a45f91a0855f35ceca88cef568e1ead88d
Simon <simonfong6@gmail.com>
2018-12-11 01:37:23 -0800

    Changed input numbers to not have default value

--- sad-score/static/js/input_number.jsx
+++ sad-score/static/js/input_number.jsx
@@ -14,7 +14,6 @@ class InputNumber extends React.Component {
           className="quiz_input"
           id={this.props.id}
           name={this.props.name}
-          value={this.props.value}
         />
         <span> {this.props.label}</span>
       </li>
