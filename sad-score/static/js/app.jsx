4719b18173b2081cab6f007dc00915ab897d1906
Simon <simonfong6@gmail.com>
2018-12-11 00:53:06 -0800

    IQ Score -> React

--- sad-score/static/js/app.jsx
+++ sad-score/static/js/app.jsx
@@ -32,34 +32,7 @@ class App extends React.Component {
           name="degree_waste_of_time"
           label="Asian parents think your degree is a waste of time."
           value="-3"/>
-        <li>
-            <input type="number" className="quiz_input" name="iq_score" id="iq" value="0"/> IQ score or estimate.
-            <br/>
-            IQ Score Meanings from this <a href="http://www.wilderdom.com/intelligence/IQWhatScoresMean.html">site</a>:
-            <ul>
-            <li>
-                Over 140 - Genius or near genius
-            </li>
-            <li>
-                120 - 140 - Very superior intelligence
-            </li>
-            <li>
-                110 - 119 - Superior intelligence
-            </li>
-            <li>
-                90 - 109 - Normal or average intelligence
-            </li>
-            <li>
-                80 - 89 - Dullness
-            </li>
-            <li>
-                70 - 79 - Borderline deficiency
-            </li>
-            <li>
-                Under 70 - Definite feeble-mindedness
-            </li>
-            </ul>
-        </li>
+        <InputIqScore/>
         <CheckboxQuiz
           name=""
           label=""
---
+++ sad-score/static/js/input_iq_score.jsx
@@ -0,0 +1,41 @@
+'use strict';
+
+class InputIqScore extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+
+    return (
+      <li>
+        <input type="number" className="quiz_input" name="iq_score" id="iq" value="0"/> IQ score or estimate.
+        <br/>
+          IQ Score Meanings from this <a href="http://www.wilderdom.com/intelligence/IQWhatScoresMean.html">site</a>:
+        <ul>
+          <li>
+            Over 140 - Genius or near genius
+          </li>
+          <li>
+            120 - 140 - Very superior intelligence
+          </li>
+          <li>
+            110 - 119 - Superior intelligence
+          </li>
+          <li>
+            90 - 109 - Normal or average intelligence
+          </li>
+          <li>
+            80 - 89 - Dullness
+          </li>
+          <li>
+            70 - 79 - Borderline deficiency
+          </li>
+          <li>
+            Under 70 - Definite feeble-mindedness
+          </li>
+        </ul>
+      </li>
+    );
+  }
+}
