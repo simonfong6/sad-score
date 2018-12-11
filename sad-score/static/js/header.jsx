d0051e6b53beeb839fbb75101bcd7fbacc75c81b
Simon <simonfong6@gmail.com>
2018-12-11 00:39:34 -0800

    Fixed indent style

--- sad-score/static/js/header.jsx
+++ sad-score/static/js/header.jsx
@@ -9,18 +9,18 @@ class Header extends React.Component {
 
     return (
       <center>
-      <h1>Subtle Asian Dating Score</h1>
-      <p>
-        Hey guys! Heard of the SAT score? Now here we have the SAD score. The SAD score measures how much the average asian mother would approve of you as their child’s significant other.
-      </p>
-      <p>
-        Quiz Version 2. Version 1 can be found <a href="/quiz/men?version=0">here</a>:
-      </p>
-      <p>
-        Credit to Ev Erest Xu for scoring system.
-        <br/>
-        Instagram: <a href="https://www.instagram.com/1ly_away/?hl=en">@1ly_away</a>
-      </p>
+        <h1>Subtle Asian Dating Score</h1>
+        <p>
+          Hey guys! Heard of the SAT score? Now here we have the SAD score. The SAD score measures how much the average asian mother would approve of you as their child’s significant other.
+        </p>
+        <p>
+          Quiz Version 2. Version 1 can be found <a href="/quiz/men?version=0">here</a>:
+        </p>
+        <p>
+          Credit to Ev Erest Xu for scoring system.
+          <br/>
+          Instagram: <a href="https://www.instagram.com/1ly_away/?hl=en">@1ly_away</a>
+        </p>
       </center>
     );
   }
